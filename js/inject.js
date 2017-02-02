7d909d4351b1ed1ba2876ea9a9a02de046f48fd0
tzafrir.raveh <tzafrir.raveh@galacoral.com>
2017-02-02 22:13:26 +0200

    hello world start of all time

---
+++ js/inject.js
@@ -0,0 +1,63 @@
+function injectForm(obj) {
+    let changeEvent = new Event('change');
+    for (let i = 0; i < obj.inputs.length; i++) {
+        if (obj.inputs[i].value !== '') {
+            try {
+                let inputField = returnDomElement('input', obj.inputs[i]);
+                inputField.value = obj.inputs[i].value;
+                inputField.dispatchEvent(changeEvent);
+            } catch (e) {
+            }
+        }
+    }
+    for (let i = 0; i < obj.selections.length; i++) {
+        try {
+            let selectField = returnDomElement('select', obj.selections[i]);
+            selectField.selectedIndex = obj.selections[i].selectIndex;
+            selectField.dispatchEvent(changeEvent);
+        } catch (e) {
+        }
+    }
+}
+function returnDomElement(type, selector) {
+    let attrs = selector.attr;
+    let querySrt = type;
+    for (let attr in attrs) {
+        if(attrs[attr] !== "") {
+            querySrt += '[' + attr + '="' + attrs[attr] + '"]';
+        }
+    }
+    return document.querySelector(querySrt);
+}
+function saveForm() {
+    function getAttr(attrList) {
+        let attrObj = {};
+        for (let i = 0; i < attrList.length; i++) {
+            attrObj[attrList[i].name] = attrList[i].nodeValue;
+        }
+        return attrObj;
+    }
+
+    let inputObj = [];
+    let selectObj = [];
+    let inputs = document.querySelectorAll('input');
+    let selections = document.querySelectorAll('select');
+
+    for (let i = 0; i < inputs.length; i++) {
+        inputObj[i] = {};
+        inputObj[i].attr = getAttr(inputs[i].attributes);
+        inputObj[i].value = inputs[i].value;
+    }
+    for (let i = 0; i < selections.length; i++) {
+        selectObj[i] = {};
+        selectObj[i].attr = getAttr(selections[i].attributes);
+        selectObj[i].selectIndex = selections[i].selectedIndex;
+    }
+    let autoFormObj = {};
+    autoFormObj.inputs = inputObj;
+    autoFormObj.selections = selectObj;
+    autoFormObj.func = 'save';
+    autoFormObj.url = document.URL;
+    chrome.runtime.sendMessage({formParam: autoFormObj}, function (response) {
+    });
+}
---
+++ js/main.js
@@ -0,0 +1,52 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.formParam.func === 'save') {
+            saveForm(request.formParam);
+        }
+        let theUrl = sender.tab.url.split('?')[0];
+        theUrl = theUrl.split('#')[0];
+        localStorage.setItem(theUrl, JSON.stringify(request.formParam));
+        alert('Form save successfully')
+
+    });
+var inject = document.getElementById('inject');
+inject.addEventListener('click', sendForm);
+var save = document.getElementById('save');
+save.addEventListener('click', bringForm);
+var del = document.getElementById('delete');
+del.addEventListener('click', deleteStorage);
+function sendForm() {
+    var tabUrl;
+    chrome.tabs.getSelected(null, function (tab) {
+        let theUrl = tab.url.split('?')[0];
+        theUrl = theUrl.split('#')[0];
+
+        asynContinufunc(theUrl);
+    });
+    function asynContinufunc(responUrl) {
+        var formObj = localStorage.getItem(responUrl);
+        if (formObj === null) {
+            chrome.tabs.executeScript(null, {code: "injectForm('empty');"});
+        } else {
+            chrome.tabs.executeScript(null, {code: "injectForm(" + formObj + ");"});
+        }
+    }
+}
+function bringForm() {
+    chrome.tabs.executeScript(null, {code: "saveForm();"});
+}
+function saveForm(obj) {
+    var tabUrl = obj.url;
+    delete obj.func;
+    delete obj.url;
+    localStorage.setItem(tabUrl, JSON.stringify(obj));
+    alert('Form save successfully')
+}
+function deleteStorage() {
+    chrome.tabs.getSelected(null, function (tab) {
+        let theUrl = tab.url.split('?')[0];
+        theUrl = theUrl.split('#')[0];
+        localStorage.removeItem(theUrl);
+        alert('delete successfully')
+    });
+}
